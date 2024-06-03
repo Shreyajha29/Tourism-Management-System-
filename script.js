77630c052673c8fc3db8db6c86c6f2edd65ebda5
Shreya Jha <90820790+Shreyajha29@users.noreply.github.com>
2024-06-03 22:42:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function exploreTours() {
+    document.getElementById('tours').scrollIntoView({ behavior: 'smooth' });
+}
+function about(){
+    document.getElementById('about').scrollIntoView({behavior: 'smooth'});
+}
+function contactUs(){
+    document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+}
